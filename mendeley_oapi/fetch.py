3ad32048e5171bd56832a13b17187bcd8c05ca38
Carles Pina i Estany <carles@pina.cat>
2013-01-18 11:47:32 +0000

    Adds the PDF files.

--- mendeley_oapi/fetch.py
+++ mendeley_oapi/fetch.py
@@ -27,11 +27,27 @@ def authorsToString(authors):
 
     return a
 
+def downloadFile(document):
+    if document.has_key('files'):
+        file_hash = document['files'][0]['file_hash']
+	document_id = document['id']
+
+        path = '/tmp/' + file_hash + '.pdf'
+        file_content = mendeley.download_file(document_id, file_hash)
+	f=open(path, 'w')
+	f.write(str(file_content))
+	f.close()
+
+	return path
+
+    return ''
+
 def getDocumentMetaInformation(document_id):
     document = mendeley.document_details(document_id)
     d = {}
     d['title'] = document['title']
     d['authors'] = authorsToString(document['authors'])
+    d['path'] = downloadFile(document)
     return d
 
 def getDocumentsMetaInformation(documents):
